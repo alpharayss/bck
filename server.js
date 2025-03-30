0cf59672dbc838036fb807948b7e2a224ff6b62b
Ankit Singh <alpharays.mail@gmail.com>
2025-03-30 13:39:57 +0530

    ddd

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000; // Change this from 80 to 3000
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
