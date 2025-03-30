d03999a2421602c444e63d3ed05e8122549610ee
Ankit Singh <alpharays.mail@gmail.com>
2025-03-30 13:31:00 +0530

    s

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3000;
+const PORT = 80;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://35.194.15.58:${PORT}`);
 });
