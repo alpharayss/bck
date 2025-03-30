cc186f8fb13a67a085793d3d89fb0f265d374abf
Ankit Singh <alpharays.mail@gmail.com>
2025-03-30 23:27:06 +0530

    sssss

--- server.js
+++ server.js
@@ -265,7 +265,7 @@ app.get('/health', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
